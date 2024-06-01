1f789f6d640e5f0a05a42be0ad45d6ecb30cf709
Joonyoung Park <jooony@snu.ac.kr>
2024-06-01 14:55:26 +0900

    [feat/refactor] Modify root layout v.2

--- app/page.tsx
+++ app/page.tsx
@@ -7,25 +7,23 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="flex justify-center h-screen bg-black text-white">
-      <div className="flex flex-col items-center justify-center gap-48">
-        <div className="w-5/6 max-w-7xl">
-          <Image
-            src="/image/title.png"
-            alt="Title"
-            width={0}
-            height={0}
-            sizes="100vw"
-            className="w-full h-full"
-          />
-        </div>
-        <button
-          className="hover:underline sm:text-3xl text-xl font-NanumMyeongjo"
-          onClick={() => router.push("/login")}
-        >
-          바로 시작하기
-        </button>
+    <div className="flex flex-col items-center justify-center gap-48">
+      <div className="w-5/6 max-w-7xl">
+        <Image
+          src="/image/title.png"
+          alt="Title"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="w-full h-full"
+        />
       </div>
+      <button
+        className="hover:underline sm:text-3xl text-xl font-NanumMyeongjo"
+        onClick={() => router.push("/login")}
+      >
+        바로 시작하기
+      </button>
     </div>
   );
 }
