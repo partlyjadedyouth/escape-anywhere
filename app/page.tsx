2d7cd19c8ae59427452d5c77544ffa4755e17874
Joonyoung Park <jooony@snu.ac.kr>
2024-06-01 14:35:10 +0900

    [Refactor] Modify layout of /home

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,35 @@ export default function Home() {
 
   return (
     <div className="flex justify-center h-screen bg-black text-white">
-      <div className="grid m-auto text-center">
+      <div className="flex flex-col items-center justify-center gap-60">
+        <div className="w-3/5">
+          <Image
+            src="/image/title.png"
+            alt="Title"
+            width={10000}
+            height={10000}
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <button
+            className={
+              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
+            }
+            onClick={() => router.push("/login")}
+          >
+            로그인 후 시작하기
+          </button>
+          <button
+            className={
+              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
+            }
+            onClick={() => router.push("/theme?guest=true")}
+          >
+            바로 시작하기
+          </button>
+        </div>
+      </div>
+      {/* <div className="z-10 grid m-auto text-center">
         <Image
           src="/image/title.png"
           alt="Title"
@@ -21,6 +49,7 @@ export default function Home() {
             className={
               "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
             }
+            onClick={() => router.push("/login")}
           >
             로그인 후 시작하기
           </button>
@@ -28,12 +57,12 @@ export default function Home() {
             className={
               "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
             }
-            onClick={() => router.push("../theme?guest=true")}
+            onClick={() => router.push("/theme?guest=true")}
           >
             바로 시작하기
           </button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
