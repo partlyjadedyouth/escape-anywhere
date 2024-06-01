3f5f3347f1939e54f213465b19f5ddc9938c6d39
Joonyoung Park <jooony@snu.ac.kr>
2024-06-01 14:15:32 +0900

    [Refactor] Modify /home

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,25 +1,17 @@
 "use client";
 
-import React from 'react';
+import React from "react";
 import { useRouter } from "next/navigation";
-import { useSession, signOut } from "next-auth/react";
 import Game from "../../components/game/page";
 
 const Main = () => {
   const router = useRouter();
-  const { data } = useSession({
-    required: true,
-    onUnauthenticated() {
-      router.replace(".//login");
-    },
-  });
-
-  // if(!data && !isGuest) {
-  //   router.replace(".//login");
-  // }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white bg-center bg-cover" style={{backgroundImage: "url('image.png')"}}>
+    <div
+      className="flex flex-col items-center justify-center min-h-screen bg-black text-white bg-center bg-cover"
+      style={{ backgroundImage: "url('image.png')" }}
+    >
       {/* 저장된 테마 정보를 불러오기 */}
       <Game />
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,39 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
-import Main from "./main/page";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <SessionProvider>
-      <div>
-        <Main />
+    <div className="flex justify-center h-screen bg-black text-white">
+      <div className="grid m-auto text-center">
+        <Image
+          src="/image/title.png"
+          alt="Title"
+          width={10000}
+          height={10000}
+          className="p-4"
+        />
+        <div className="flex flex-col items-center justify-center">
+          <button
+            className={
+              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
+            }
+          >
+            로그인 후 시작하기
+          </button>
+          <button
+            className={
+              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
+            }
+            onClick={() => router.push("../theme?guest=true")}
+          >
+            바로 시작하기
+          </button>
+        </div>
       </div>
-    </SessionProvider>
+    </div>
   );
 }
