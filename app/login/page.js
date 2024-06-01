30c206979ba21d23197cc3aafb8e33d15f2eeb77
Joonyoung Park <jooony@snu.ac.kr>
2024-06-01 12:03:49 +0900

    [main] Fix errors and warnings on deploy

--- app/login/page.js
+++ app/login/page.js
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 
 export default function Login() {
   const router = useRouter();
@@ -11,38 +12,55 @@ export default function Login() {
     <div className="flex justify-center h-screen bg-black text-white">
       {session ? (
         <div className="grid m-auto text-center">
-          <img src="title.png" alt="Escape Anywhere" className="m-4" />
+          <Image src="title.png" alt="Escape Anywhere" className="m-4" />
           <div className="flex flex-col items-center justify-center">
-            <button className={
-              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"}
-              onClick={() => router.push('/')}
-              /* main으로 돌아가서 Game 실행 */>
+            <button
+              className={
+                "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
+              }
+              onClick={() => router.push("/")}
+              /* main으로 돌아가서 Game 실행 */
+            >
               이어하기
             </button>
-            <button className={
-              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"}
-              onClick={() => router.push('../theme')}
-              /* Theme 실행 */>
+            <button
+              className={
+                "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
+              }
+              onClick={() => router.push("../theme")}
+              /* Theme 실행 */
+            >
               새로 시작하기
             </button>
-            <button className="w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none" onClick={() => signOut()}>로그아웃</button>
+            <button
+              className="w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
+              onClick={() => signOut()}
+            >
+              로그아웃
+            </button>
           </div>
         </div>
       ) : (
         <div className="grid m-auto text-center">
-          <img src="title.png" alt="Escape Anywhere" className="m-4" />
+          <Image src="title.png" alt="Escape Anywhere" className="m-4" />
           <div className="flex flex-col items-center justify-center">
-            <button className={
-              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"}
-              onClick={() => signIn()}>
+            <button
+              className={
+                "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
+              }
+              onClick={() => signIn()}
+            >
               로그인 후 시작하기
             </button>
-            <button className={
-              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"}
-              onClick={() => router.push('../theme?guest=true')}>
+            <button
+              className={
+                "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
+              }
+              onClick={() => router.push("../theme?guest=true")}
+            >
               바로 시작하기
             </button>
-         </div>
+          </div>
         </div>
       )}
     </div>
--- app/theme/page.tsx
+++ app/theme/page.tsx
@@ -1,37 +1,48 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
 const Theme = () => {
   const router = useRouter();
 
-  const [theme, setTheme] = useState({ value: '', selected: false });
-  const [level, setLevel] = useState({ value: '', selected: false });
+  const [theme, setTheme] = useState({ value: "", selected: false });
+  const [level, setLevel] = useState({ value: "", selected: false });
 
-  const handleThemeSubmit = (event) => {
+  const handleThemeSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setTheme({ ...theme, selected: true });
   };
 
-  const handleLevelSelect = (selectedLevel) => {
+  const handleLevelSelect = (selectedLevel: string) => {
     setLevel({ value: selectedLevel, selected: true });
   };
 
   switch (true) {
     case !theme.selected:
       return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white bg-center bg-cover" style={{backgroundImage: "url('background.png')"}}>
-          <h1 className="text-center text-2xl font-bold mb-4">원하는 방탈출 컨셉을 설명해주세요</h1>
-          <form onSubmit={handleThemeSubmit} className="w-3/4 flex justify-between">
-            <input 
-              className="w-full h-10 p-4 mb-4 bg-white text-black" 
-              value={theme.value} 
-              onChange={(e) => setTheme({ ...theme, value: e.target.value })} 
+        <div
+          className="flex flex-col items-center justify-center min-h-screen bg-black text-white bg-center bg-cover"
+          style={{ backgroundImage: "url('background.png')" }}
+        >
+          <h1 className="text-center text-2xl font-bold mb-4">
+            원하는 방탈출 컨셉을 설명해주세요
+          </h1>
+          <form
+            onSubmit={handleThemeSubmit}
+            className="w-3/4 flex justify-between"
+          >
+            <input
+              className="w-full h-10 p-4 mb-4 bg-white text-black"
+              value={theme.value}
+              onChange={(e) => setTheme({ ...theme, value: e.target.value })}
             />
-            <button className={ 
-              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"}
-              type="submit">
+            <button
+              className={
+                "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
+              }
+              type="submit"
+            >
               전송
             </button>
           </form>
@@ -39,22 +50,30 @@ const Theme = () => {
       );
     case !level.selected:
       return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white bg-center bg-cover" style={{backgroundImage: "url('background.png')"}}>
-          <h1 className="text-center text-2xl font-bold mb-4">원하는 난이도를 선택해주세요</h1>
+        <div
+          className="flex flex-col items-center justify-center min-h-screen bg-black text-white bg-center bg-cover"
+          style={{ backgroundImage: "url('background.png')" }}
+        >
+          <h1 className="text-center text-2xl font-bold mb-4">
+            원하는 난이도를 선택해주세요
+          </h1>
           <div className="w-3/4 flex justify-between">
-            <button 
+            <button
               className="w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
-              onClick={() => handleLevelSelect('쉬움')}>
+              onClick={() => handleLevelSelect("쉬움")}
+            >
               쉬움
             </button>
-            <button 
+            <button
               className="w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
-              onClick={() => handleLevelSelect('보통')}>
+              onClick={() => handleLevelSelect("보통")}
+            >
               보통
             </button>
-            <button 
+            <button
               className="w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
-              onClick={() => handleLevelSelect('어려움')}>
+              onClick={() => handleLevelSelect("어려움")}
+            >
               어려움
             </button>
           </div>
@@ -62,12 +81,23 @@ const Theme = () => {
       );
     default:
       return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white bg-center bg-cover" style={{backgroundImage: "url('background.png')"}}>
-          <h1 className="text-center text-2xl font-bold mb-4">지금부터 이야기 속에 숨겨진 문제를 풀어 차례대로 모든 방을 탈출하시면 성공입니다.</h1>
-          <h1 className="text-center text-2xl font-bold mb-4">행운을 빕니다.</h1>
-          <button className={ 
-            "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"}
-            onClick={() => router.push('/')}>
+        <div
+          className="flex flex-col items-center justify-center min-h-screen bg-black text-white bg-center bg-cover"
+          style={{ backgroundImage: "url('background.png')" }}
+        >
+          <h1 className="text-center text-2xl font-bold mb-4">
+            지금부터 이야기 속에 숨겨진 문제를 풀어 차례대로 모든 방을
+            탈출하시면 성공입니다.
+          </h1>
+          <h1 className="text-center text-2xl font-bold mb-4">
+            행운을 빕니다.
+          </h1>
+          <button
+            className={
+              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
+            }
+            onClick={() => router.push("/")}
+          >
             시작하기
           </button>
         </div>
