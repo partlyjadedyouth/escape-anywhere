22a39945fdd6a086e7a41ce4b8446f332e2e0993
Joonyoung Park <jooony@snu.ac.kr>
2024-06-01 14:46:36 +0900

    [feat/refactor] Add new font face NanumMyeongjo

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   return (
     <div className="flex justify-center h-screen bg-black text-white">
-      <div className="flex flex-col items-center justify-center gap-60">
+      <div className="flex flex-col items-center justify-center gap-48">
         <div className="w-3/5">
           <Image
             src="/image/title.png"
@@ -17,52 +17,13 @@ export default function Home() {
             height={10000}
           />
         </div>
-        <div className="flex flex-col items-center">
-          <button
-            className={
-              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
-            }
-            onClick={() => router.push("/login")}
-          >
-            로그인 후 시작하기
-          </button>
-          <button
-            className={
-              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
-            }
-            onClick={() => router.push("/theme?guest=true")}
-          >
-            바로 시작하기
-          </button>
-        </div>
+        <button
+          className="hover:underline sm:text-3xl text-xl font-NanumMyeongjo"
+          onClick={() => router.push("/login")}
+        >
+          바로 시작하기
+        </button>
       </div>
-      {/* <div className="z-10 grid m-auto text-center">
-        <Image
-          src="/image/title.png"
-          alt="Title"
-          width={10000}
-          height={10000}
-          className="p-4"
-        />
-        <div className="flex flex-col items-center justify-center">
-          <button
-            className={
-              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
-            }
-            onClick={() => router.push("/login")}
-          >
-            로그인 후 시작하기
-          </button>
-          <button
-            className={
-              "w-40 py-2 px-4 bg-transparent hover:underline focus:outline-none"
-            }
-            onClick={() => router.push("/theme?guest=true")}
-          >
-            바로 시작하기
-          </button>
-        </div>
-      </div> */}
     </div>
   );
 }
