fc2d4b002d5eb6f455445e7d96313182c1e7e50e
Joonyoung Park <jooony@snu.ac.kr>
2024-06-08 14:19:54 +0900

    [feat/generate-image] Add placeholder image to handle error

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: NextRequest) {
   try {
     const response = await openai.images.generate({
       // OpenAI의 DALL-E 모델을 사용하여 이미지를 생성합니다.
-      model: "dall-e-4", // 사용할 모델을 지정합니다.
+      model: "dall-e-3", // 사용할 모델을 지정합니다.
       prompt: text, // 이미지 생성을 위한 프롬프트를 설정합니다.
       n: 1, // 생성할 이미지의 수를 설정합니다.
       size: "1024x1024", // 이미지의 크기를 설정합니다.
