f79bb3e0149014ea5f60ada3ddf8e9fb0214a7df
vivapyk <vivapyk@gmail.com>
2024-06-09 15:40:59 +0900

    add type

--- app/ending/page.tsx
+++ app/ending/page.tsx
@@ -30,7 +30,7 @@ export default function Ending() {
     return <div>Invalid time parameter</div>;
   }
 
-  const formatTime = (milliseconds) => {
+  const formatTime = (milliseconds: number) => {
     const totalSeconds = Math.floor(milliseconds / 1000);
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
