8f400604ce1c4e230feef4811627276e4f715fe7
Joonyoung Park <jooony@snu.ac.kr>
2024-06-15 22:21:12 +0900

    [main] Remove console.log-s

--- lib/utils/generateImage.ts
+++ lib/utils/generateImage.ts
@@ -1,18 +1,18 @@
 export const generateImage = async (text: string) => {
-    console.log(text)
-    try {
-        const response = await fetch("/api/image", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ text }),
-        });
-        console.log(response)
-        const data = await response.json();
-        console.log(data.imageUrl)
-        return data.imageUrl;
-    } catch (error) {
-        console.error("Error generating image:", error);
-    }
+  //   console.log(text);
+  try {
+    const response = await fetch("/api/image", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text }),
+    });
+    // console.log(response)
+    const data = await response.json();
+    // console.log(data.imageUrl)
+    return data.imageUrl;
+  } catch (error) {
+    console.error("Error generating image:", error);
+  }
 };
